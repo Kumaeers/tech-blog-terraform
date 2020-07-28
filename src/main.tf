d2715a9180e76f8fdb395156a4679b6dfff67bc0
Kobatai <taiga1983346@gmail.com>
2020-07-28 13:57:53 +0900

    add:DB接続情報をSSMパラメータストアで管理

--- src/main.tf
+++ src/main.tf
@@ -676,3 +676,26 @@ module "ecs_task_execution_role" {
 #     }
 #   }
 # }
+
+# DB用のSSMパラメータストア設定
+resource "aws_ssm_parameter" "db_username" {
+  name        = "/db/username"
+  value       = "root"
+  type        = "String"
+  description = "データベースのユーザー名"
+}
+
+# このままだと暗号化すべきパスワードがバージョン管理されてしまう
+# Terraform ではダミー値を設定して、あとでAWS CLI から更新するという戦略を取る
+# 好みの問題だがSSMパラメータはTerraform管理しなくても良いが、
+# 外部サービスのクレデンシャルの発行方法など、ロストしやすい情報をコメントとして残す場所として使うには最適
+resource "aws_ssm_parameter" "db_raw_password" {
+  name        = "/db/password"
+  value       = "uninitialized"
+  type        = "SecureString"
+  description = "データベースのパスワード"
+
+  lifecycle {
+    ignore_changes = [value]
+  }
+}
