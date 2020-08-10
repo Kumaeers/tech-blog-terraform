c6a71f16823dd5ace27a7a1bff2d4e7c294d342b
Kobatai <taiga1983346@gmail.com>
2020-08-10 17:47:48 +0900

    add:ECR反映

--- src/ecr.tf
+++ src/ecr.tf
@@ -1,29 +1,29 @@
-# # ECR
-# resource "aws_ecr_repository" "example" {
-#   name = "example"
-# }
+# ECR
+resource "aws_ecr_repository" "tech-blog" {
+  name = "tech-blog"
+}
 
-# # ライフサイクルポリシーでイメージの数が増えすぎないように制御
-# resource "aws_ecr_lifecycle_policy" "example" {
-#   repository = aws_ecr_repository.example.name
+# ライフサイクルポリシーでイメージの数が増えすぎないように制御
+resource "aws_ecr_lifecycle_policy" "tech-blog" {
+  repository = aws_ecr_repository.tech-blog.name
 
-#   policy = <<EOF
-#   {
-#     "rules": [
-#       {
-#         "rulePriority": 1,
-#         "description": "Keep last 30 release tagged images",
-#         "selection": {
-#           "tagStatus": "tagged",
-#           "tagPrefixList": ["release"],
-#           "countType": "imageCountMoreThan",
-#           "countNumber": 30
-#         },
-#         "action": {
-#           "type": "expire"
-#         }
-#       }
-#     ]
-#   }
-#   EOF
-# }
+  policy = <<EOF
+  {
+    "rules": [
+      {
+        "rulePriority": 1,
+        "description": "Keep last 30 release tagged images",
+        "selection": {
+          "tagStatus": "tagged",
+          "tagPrefixList": ["release"],
+          "countType": "imageCountMoreThan",
+          "countNumber": 30
+        },
+        "action": {
+          "type": "expire"
+        }
+      }
+    ]
+  }
+  EOF
+}
